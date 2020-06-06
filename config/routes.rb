7381210b948427eb8026733fec38c13d1b1ab662
sho358 <ishikawa.hokudai.2013@gmail.com>
2020-06-07 08:29:38 +0900

    新規料理投稿画面作成

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   get :signup,  to: 'users#new'
   resources :users
+  resources :dishes
 
   get :login,  to: 'sessions#new'
   post :login,  to: 'sessions#create'
