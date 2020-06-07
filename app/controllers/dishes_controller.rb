8ebab785255cc056c1b9b826eef855454227d3f6
sho358 <ishikawa.hokudai.2013@gmail.com>
2020-06-07 09:40:43 +0900

    料理の更新機能実装

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -19,6 +19,21 @@ class DishesController < ApplicationController
       end
     end
 
+    def edit 
+      @dish = Dish.find(params[:id])
+    end
+
+    def update 
+      @dish = Dish.find(params[:id])
+      if @dish.update_attributes(dish_params)
+        flash[:success] = "料理情報が更新されました！"
+        redirect_to @dish
+      else
+        render 'edit'
+      end
+    end
+
+
     private 
 
       def dish_params
